f39500d8a8667a8758f9ff3ca8dd72c8c49f8ccb
Gerardo Galindez <ggalindezb@gmail.com>
2019-02-21 12:09:10 -0600

    Add proper text asset handling

--- text/manager.rb
+++ text/manager.rb
@@ -2,19 +2,26 @@ module Text
   # Handle text operations
   class Manager
     PATH = 'text/'
-    ENTRIES = {
-      banner: 'graphics/banner.txt'
+    BLOCK = {
+      help: 'help.txt'
+    }.freeze
+    GRAPHIC = {
+      banner: 'banner.txt',
+      banner_2: 'banner_2.txt'
     }.freeze
 
     class << self
-      def fetch_graphic(entry)
-        File.read(file_path(entry))
+      %i(block graphic).each do |type|
+        define_method "fetch_#{type}".to_sym do |entry|
+          File.read(file_path(type, entry))
+        end
       end
 
       private
 
-      def file_path(entry)
-        PATH + ENTRIES[entry]
+      def file_path(type, entry)
+        asset = const_get(type.upcase)[entry]
+        File.join(PATH, type.to_s, asset) unless asset.nil?
       end
     end
   end
