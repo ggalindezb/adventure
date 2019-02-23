e40dfab32a914a0545a9bca96eeada48a600ec13
Gerardo Galindez <ggalindezb@gmail.com>
2019-02-22 21:52:49 -0600

    - Add convention over config for text loading

--- text/manager.rb
+++ text/manager.rb
@@ -2,26 +2,27 @@ module Text
   # Handle text operations
   class Manager
     PATH = 'text/'
-    BLOCK = {
-      help: 'help.txt'
-    }.freeze
-    GRAPHIC = {
-      banner: 'banner.txt',
-      banner_2: 'banner_2.txt'
-    }.freeze
 
     class << self
+      def story(part, section)
+        YAML.load(File.read(file_path))
+      end
+
       %i(block graphic).each do |type|
         define_method "fetch_#{type}".to_sym do |entry|
-          File.read(file_path(type, entry))
+          begin
+            File.read(file_path(type, entry))
+          rescue Errno::ENOENT => e
+            missing_file = e.message.split('-').last.strip
+            "Missing file: #{missing_file}"
+          end
         end
       end
 
       private
 
-      def file_path(type, entry)
-        asset = const_get(type.upcase)[entry]
-        File.join(PATH, type.to_s, asset) unless asset.nil?
+      def file_path(type, entry, extension='txt')
+        File.join(PATH, type.to_s, "#{entry}.#{extension}")
       end
     end
   end
